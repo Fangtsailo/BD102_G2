70091623d64faff6e13744a1c0ae24a98738c267
joan <jeanke1116@gmail.com>
2017-10-12 20:56:23 +0800

    content增設height: 64vh;解決footer上移

--- memcomment.php
+++ memcomment.php
@@ -5,9 +5,11 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
 	<title>會員專區</title>
+	<link rel="stylesheet" type="text/css" href="css/basic.css">
 	<link rel="stylesheet" type="text/css" href="css/grid.css">
 	<link rel="stylesheet" type="text/css" href="css/memtry.css">
 	<script type="text/javascript" src="js/header.js"></script>
+
 
 </head>
 <body>
