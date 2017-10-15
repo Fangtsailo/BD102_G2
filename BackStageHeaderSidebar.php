b011d51614e3e825efc9da80cd2c02a0b5164045
adamLo <spm770311@gmail.com>
2017-10-15 09:54:44 +0800

    back stage header update

--- BackStageHeaderSidebar.php
+++ BackStageHeaderSidebar.php
@@ -6,6 +6,24 @@
 				<img src="img/trepunLogoWhite.svg">
 			</div>
 			<p>後台管理系統</p>
+			<ul class="navition">
+				<a href="#"><li>後台管理</li></a>
+				<a href="#"><li class="memMan"><span>會員管理</span>
+					<ul>
+						<a href="#"><li>一般會員</li></a>
+						<a href="#"><li>店長會員</li></a>
+					</ul>
+				</li></a>
+				<a href="#"><li class="bsnMan"><span>商家管理</span>
+					<ul>
+						<a href="#"><li>店家審核</li></a>
+						<a href="#"><li>店面資訊</li></a>
+						<a href="#"><li>胖小車資訊</li></a>
+					</ul>
+				</li></a>
+				<a href="#"><li>留言管理</li></a>
+				<a href="#"><li>活動管理</li></a>
+			</ul>
 		</div>
 		
 
@@ -22,7 +40,7 @@
 
 <!-- 	<div class="container"> -->
 	<!-- 	<div class="coverNavition"> -->
-			<div class="navition">
+			<!-- <div class="navition">
 				<ul>
 					<a href=""><li>後台管理員</li></a>
 					<a href=""><li>會員管理</li></a>
@@ -35,4 +53,4 @@
 					<a href=""><li>留言管理</li></a>
 					<a href=""><li>體驗活動管理</li></a>
 				</ul>
-			</div>
+			</div> -->
