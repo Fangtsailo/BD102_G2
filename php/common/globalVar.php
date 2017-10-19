988d633db53cd76603c43f2f1ab177e149fbae63
changelin <linchienchunprivate@gmail.com>
2017-10-19 19:56:18 +0800

    test

--- php/common/globalVar.php
+++ php/common/globalVar.php
@@ -1,4 +1,5 @@
 <?php 
 	define("GLOBAL_MEM_PIC_PATH", "img/member_pic/");//會員大頭照路徑
 	define("GLOBAL_STORE_PIC_PATH", "img/store/logo/");//店家logo路徑
+	define("GLOBAL_PRODUCTS_PIC_PATH", "img/store/products/");//商品logo路徑
  ?>
