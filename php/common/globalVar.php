15ffd1e9402badbd12f29967c6681633eacfbddb
kingshoes <31884390+kingshoes@users.noreply.github.com>
2017-10-25 16:02:28 +0800

    Update globalVar.php

--- php/common/globalVar.php
+++ php/common/globalVar.php
@@ -1,14 +1,9 @@
 <?php 
 	define("GLOBAL_MEM_PIC_PATH", "img/member_pic/");//會員大頭照路徑
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-	define("GLOBAL_STORE_PIC_PATH", "img/store/logo/");//店家logo路徑
->>>>>>> 98f266ad1ea474ec423417d49223b7bb087afed7
-=======
 	define("GLOBAL_STORE_PIC_PATH", "img/store/logo/");//店家logo路徑
 	define("GLOBAL_PRODUCTS_PIC_PATH", "img/store/products/");//商品logo路徑
-	define("GLOBAL_ACTIVITY_PIC_PATH", "img/store/activity/banner/");//活動banner圖片路徑
+	define("GLOBAL_ACTIVITY_PIC_PATH", "img/store/activity/banner/");//活動-banner圖片路徑
+	define("GLOBAL_ACTIVITY_PRODUCT_PIC_PATH", "img/store/activity/product/");//活動-成品圖片路徑
+	define("GLOBAL_ACTIVITY_ACT_PIC_PATH", "img/store/activity/activity/");//活動-活動圖片路徑
 	define("GLOBAL_STORE_BANNERS_PIC_PATH", "img/store/banners/");//店家banner圖片路徑
->>>>>>> b3bcac46288a9bc74471c7f9be36fb8dd4e39f50
  ?>
