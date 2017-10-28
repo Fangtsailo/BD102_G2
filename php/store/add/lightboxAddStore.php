d3038414b64616442a38bba455982671354cc6f8
jeremidee <jeremidee@gmail.com>
2017-10-28 16:53:31 +0800

    更新搜尋地區判斷

--- php/store/add/lightboxAddStore.php
+++ php/store/add/lightboxAddStore.php
@@ -12,11 +12,79 @@ session_start();
 	
 <?php
 try {
+			$shopPosition= mb_substr($_REQUEST["address"],0,2,"UTF-8");
+			$position = 0;
+						switch($shopPosition){
+							case '桃園':
+								$position= 0;
+								break;
+							case '台北':
+								$position= 0;
+								break;
+							case '臺北':
+								$position= 0;
+								break;
+							case '新北':
+								$position= 0;
+								break;	
+							case '宜蘭':
+								$position= 0;
+								break;
+							case '基隆':
+								$position= 0;
+								break;
+							case '新竹':
+								$position= 0;
+								break;
+							case '苗栗':
+								$position= 1;
+								break;	
+							case '台中':
+								$position= 1;
+								break;
+							case '臺中':
+								$position= 1;
+								break;
+							case '彰化':
+								$position= 1;
+								break;
+							case '雲林':
+								$position= 1;
+								break;
+							case '南投':
+								$position= 1;
+								break;
+							case '嘉義':
+								$position= 2;
+								break;
+							case '台南':
+								$position= 2;
+								break;
+							case '臺南':
+								$position= 2;
+								break;
+							case '高雄':
+								$position= 2;
+								break;
+							case '屏東':
+								$position= 2;
+								break;
+							case '台東':
+								$position= 3;
+								break;
+							case '臺東':
+								$position= 3;
+								break;
+							case '花蓮':
+								$position= 3;
+								break;
+						} //switch
+
 		require_once("../../PDO/connectPDO.php");
 		//啟動交易管理
 		$connectPDO->beginTransaction();
 		//寫入主檔
-		$newShopSQL = "insert into store_imformation (SI_MEMNO,SI_NAME,SI_STORY,SI_ADDR,SI_STARTTIME,SI_ENDTIME,SI_TYPE,SI_PHONE,SI_ADDDATE) values (:memNo,:storeName,:story,:address,:startTime,:endTime,:storeType,:tel,:addDate)";
+		$newShopSQL = "insert into store_imformation (SI_MEMNO,SI_NAME,SI_STORY,SI_ADDR,SI_STARTTIME,SI_ENDTIME,SI_TYPE,SI_PHONE,SI_ADDDATE,SI_POSITION) values (:memNo,:storeName,:story,:address,:startTime,:endTime,:storeType,:tel,:addDate,:position)";
 		$addShop = $connectPDO->prepare( $newShopSQL );
 		$addShop->bindValue(":memNo" ,$_SESSION["memNo"] );
 		$addShop->bindValue(":storeName" ,$_REQUEST["storeName"] );
@@ -27,6 +95,7 @@ try {
 		$addShop->bindValue(":endTime" ,$_REQUEST["endTime"] );
 		$addShop->bindValue(":storeType" ,$_REQUEST["storeType"] );
 		$addShop->bindValue(":addDate" ,$_REQUEST["addShopDate"] );
+		$addShop->bindValue(":position" ,$position );
 		$addShop->execute();
 		$connectPDO->commit(); //確認交易完成
 		header("Location:../../../homepage.php");
