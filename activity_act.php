9debc7bf7404ff56784abbef56198b3bd6b59a6a
Linda <a0187123@gmail.com>
2017-10-29 00:24:40 +0800

    活動假資料OK

--- activity_act.php
+++ activity_act.php
@@ -78,30 +78,28 @@ session_start();
 		<script type="text/javascript">
 			$(document).ready(function () {
 
-				$('.activity_banner').css('background','url("<?php echo $actRow->AC_BANNER1=="" ? GLOBAL_ACTIVITY_PIC_PATH."default.png" : GLOBAL_ACTIVITY_PIC_PATH.$actRow->AC_BANNER1; ?>") center center').css('background-size','cover');
+				$('.activity_banner').css('background','url("<?php echo $actRow->AC_BANNER1=="" ? GLOBAL_ACTIVITY_PIC_PATH."default.jpg" : GLOBAL_ACTIVITY_PIC_PATH.$actRow->AC_BANNER1; ?>") center center').css('background-size','cover');
 
-				$('#activity_application').after().css('background','url("<?php echo $actRow->AC_BANNER2=="" ? GLOBAL_ACTIVITY_PIC_PATH."default.png" : GLOBAL_ACTIVITY_PIC_PATH.$actRow->AC_BANNER2; ?>") center center' ).css('background-size','cover');
+				$('.activity_bgd').css('background','url("<?php echo $actRow->AC_BANNER2=="" ? GLOBAL_ACTIVITY_PIC_PATH."default.jpg" : GLOBAL_ACTIVITY_PIC_PATH.$actRow->AC_BANNER2; ?>") center center' ).css('background-size','cover');
 
-				$('.activity_dough_1').css('background','url("<?php echo $actRow->AC_PIC1=="" ? GLOBAL_ACTIVITY_ACT_PIC_PATH."" : GLOBAL_ACTIVITY_ACT_PIC_PATH.$actRow->AC_PIC1; ?>") center center').css('background-size','cover');
+				$('.activity_dough_1').css('background','url("<?php echo $actRow->AC_PIC1=="" ? GLOBAL_ACTIVITY_ACT_PIC_PATH."default1.jpg" : GLOBAL_ACTIVITY_ACT_PIC_PATH.$actRow->AC_PIC1; ?>") center center').css('background-size','cover');
 
-				$('.pic_1').css('background','url("<?php echo GLOBAL_ACTIVITY_INGREDIENT_PIC_PATH.$actRow->AC_INGREDIENT_PIC1; ?>") center center').css('background-size','cover');
+				$('.pic_1').css('background','url("<?php echo $actRow->AC_INGREDIENT_PIC1=="" ? GLOBAL_ACTIVITY_INGREDIENT_PIC_PATH."default1.jpg" : GLOBAL_ACTIVITY_INGREDIENT_PIC_PATH.$actRow->AC_INGREDIENT_PIC1; ?>") center center').css('background-size','cover');
 
-				$('.pic_2').css('background','url("<?php echo GLOBAL_ACTIVITY_INGREDIENT_PIC_PATH.$actRow->AC_INGREDIENT_PIC2; ?>") center center').css('background-size','cover');
-
-
-
-				$('.pic_3').css('background','url("<?php echo GLOBAL_ACTIVITY_INGREDIENT_PIC_PATH.$actRow->AC_INGREDIENT_PIC3; ?>") center center').css('background-size','cover');
+				$('.pic_2').css('background','url("<?php echo $actRow->AC_INGREDIENT_PIC2=="" ? GLOBAL_ACTIVITY_INGREDIENT_PIC_PATH."default2.jpg" : GLOBAL_ACTIVITY_INGREDIENT_PIC_PATH.$actRow->AC_INGREDIENT_PIC2; ?>") center center').css('background-size','cover');
 
 
-				$('.activity_dough_5').css('background','url("<?php echo GLOBAL_ACTIVITY_ACT_PIC_PATH.$actRow->AC_PIC2; ?>") center center').css('background-size','cover');
-
-				$('.activity_dough_6').css('background','url("<?php echo GLOBAL_ACTIVITY_ACT_PIC_PATH.$actRow->AC_PIC3; ?>") center center').css('background-size','cover');
+				$('.pic_3').css('background','url("<?php echo $actRow->AC_INGREDIENT_PIC3=="" ? GLOBAL_ACTIVITY_INGREDIENT_PIC_PATH."default3.jpg" : GLOBAL_ACTIVITY_INGREDIENT_PIC_PATH.$actRow->AC_INGREDIENT_PIC3; ?>") center center').css('background-size','cover');
 
 
-				$('.fin_1').css('background','url("<?php echo GLOBAL_ACTIVITY_PRODUCT_PIC_PATH.$actRow->AC_PRODUCT_PIC1; ?>") center center').css('background-size','cover');
+				$('.activity_dough_5').css('background','url("<?php echo $actRow->AC_PIC2=="" ? GLOBAL_ACTIVITY_ACT_PIC_PATH."default2.jpg" : GLOBAL_ACTIVITY_ACT_PIC_PATH.$actRow->AC_PIC2; ?>") center center').css('background-size','cover');
 
-				$('.fin_2').css('background','url("<?php echo GLOBAL_ACTIVITY_PRODUCT_PIC_PATH.$actRow->AC_PRODUCT_PIC2; ?>") center center').css('background-size','cover');
-				$('.fin_3').css('background','url("<?php echo GLOBAL_ACTIVITY_PRODUCT_PIC_PATH.$actRow->AC_PRODUCT_PIC3; ?>") center center').css('background-size','cover');
+				$('.activity_dough_6').css('background','url("<?php echo $actRow->AC_PIC3=="" ? GLOBAL_ACTIVITY_ACT_PIC_PATH."default3.jpg" : GLOBAL_ACTIVITY_ACT_PIC_PATH.$actRow->AC_PIC3; ?>") center center').css('background-size','cover');
+
+				$('.fin_1').css('background','url("<?php echo $actRow->AC_PRODUCT_PIC1=="" ? GLOBAL_ACTIVITY_PRODUCT_PIC_PATH."default1.jpg" :GLOBAL_ACTIVITY_PRODUCT_PIC_PATH.$actRow->AC_PRODUCT_PIC1; ?>") center center').css('background-size','cover');
+
+				$('.fin_2').css('background','url("<?php echo $actRow->AC_PRODUCT_PIC2=="" ? GLOBAL_ACTIVITY_PRODUCT_PIC_PATH."default2.jpg" :GLOBAL_ACTIVITY_PRODUCT_PIC_PATH.$actRow->AC_PRODUCT_PIC2; ?>") center center').css('background-size','cover');
+				$('.fin_3').css('background','url("<?php echo $actRow->AC_PRODUCT_PIC3=="" ? GLOBAL_ACTIVITY_PRODUCT_PIC_PATH."default3.jpg" : GLOBAL_ACTIVITY_PRODUCT_PIC_PATH.$actRow->AC_PRODUCT_PIC3; ?>") center center').css('background-size','cover');
 
 
 			});
@@ -151,10 +149,10 @@ session_start();
    				<ul>
    					<li>體驗地點 :<?php echo $actRow->AC_ADDRESS ?></li>
 	   				<li>時間 : <?php echo $actRow->AC_TIME ?></li>
-	   				<li>人數 : <?php echo $actRow->AC_MEM_COUNT ?>人</li>
+	   				<li>人數 : <?php echo $actRow->AC_MEM_COUNT=="" ? "由店家決定" :  $actRow->AC_MEM_COUNT ?>人</li>
 	   				<li>聯絡方式 : <?php echo $actRow->SI_PHONE ?></li>
-	   				<li>材料 : <?php echo $actRow->AC_INGREDIENT ?></li>
-	   				<li>費用 : <?php echo $actRow->AC_PRICE ?></li>
+	   				<li>材料 : <?php echo $actRow->AC_INGREDIENT=="" ? "由店家提供" : $actRow->AC_INGREDIENT ?></li>
+	   				<li>費用 : <?php echo $actRow->AC_PRICE=="" ? "詳情請聯絡店家" :  $actRow->AC_PRICE ?></li>
 	   			</ul>
    			</div>	
 					
@@ -177,7 +175,7 @@ session_start();
 			<div class="activity_pot">
 				<div class="acty_out_pot">
 					<img src="img/act/acty_pot.png"><!-- 外鍋 -->
-					<h2><?php echo $actRow->AC_SECOND_NAME ?></h2>
+					<h2><?php echo $actRow->AC_SECOND_NAME=="" ? "麵包師傅親自指導" : $actRow->AC_SECOND_NAME ?></h2>
 				 	<div class="activity_pot_b">  <!-- 最裡層 -->
 						<img src="img/act/acty_pot_b.png">
 					</div>
@@ -192,7 +190,7 @@ session_start();
 			<div class="activityPot rwdw">
 				<div class="acty_out_pot">
 					<img src="img/act/acty_pot.png"><!-- 外鍋 -->
-					<h2><?php echo $actRow->AC_SECOND_NAME ?></h2>
+					<h2><?php echo $actRow->AC_SECOND_NAME=="" ? "麵包師傅親自指導" : $actRow->AC_SECOND_NAME ?></h2>
 				 	<div class="activity_pot_b">  <!-- 最裡層 -->
 						<img src="img/act/acty_pot_b.png">
 					</div>
@@ -210,7 +208,7 @@ session_start();
 				<div class="clearfix rwd_none"></div>
 				
 				<div class="activity_dough_2">
-					<p><?php echo $actRow->AC_DETAIL2 ?>		
+					<p><?php echo $actRow->AC_DETAIL2=="" ? "烘焙坊教你製作好吃又簡單的麵包，機會難得快來體驗。" : $actRow->AC_DETAIL2 ?>		
 					</p>
 					<div class="act_btn">
 						<a href="#apply">報名</a>
@@ -221,7 +219,7 @@ session_start();
 			<div class="clearfix"></div>
 			<div class="activity_dough_3 trigger4">
 				<h2>食材選用</h2>
-				<p><?php echo $actRow->AC_DETAIL3 ?></p>
+				<p><?php echo $actRow->AC_DETAIL3=="" ? "店家食材報你知，選用讓麵包更加好吃的新鮮食材，並且安全衛生，讓你做得好吃，也吃得安心。" : $actRow->AC_DETAIL3 ?></p>
 
 	
 			</div>
@@ -268,6 +266,7 @@ session_start();
 			
 		<section class="section_6"><a name="apply">
 			<div class="activity_application trigger7" id="activity_application">
+			<div class="activity_bgd"></div>
 				<div class="activity_final">
 					<div class="activity_fin_pic fin_1">
 						
@@ -296,9 +295,6 @@ session_start();
 				</div>
 
 
-
-
-
 				<div class="activity_form">
 					<!-- =================表單樣式===================== -->
 					
@@ -321,16 +317,13 @@ session_start();
 										 
 											<div class="globalFormHeader">
 											
-					
-						
+											
 												<h1>已報名本活動</h1>
 												
 											</div>
 											<div class="globalFormContent">
-
 												
-								
-												
+																				
 												<div class="globalFormBtns">
 													
 													<p>活動地點 :  <?php echo $actRow->AC_ADDRESS ?></p>
@@ -357,8 +350,7 @@ session_start();
 
 
 											<div class="globalFormHeader">
-											
-					
+														
 						
 												<h1>報名去</h1>
 												<p>快來一起參與烘培體驗吧!會員只要填寫基本資料，就能成功報名囉! <br> 名額有限速速行動~~ </p>
