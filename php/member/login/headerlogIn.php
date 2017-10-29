09a1742ad54262036ca7f89735706695fefcf675
jeremidee <jeremidee@gmail.com>
2017-10-29 20:32:41 +0800

    no message

--- php/member/login/headerlogIn.php
+++ php/member/login/headerlogIn.php
@@ -20,19 +20,7 @@ try{
 		$_SESSION["memId"] = $memRow->MEM_ID;
 		$_SESSION["memName"] = $memRow->MEM_NAME;
 	    $_SESSION["memRole"] = $memRow->MEM_ROLE;
-<<<<<<< HEAD
 	    $_SESSION["memPic"] = ($memRow->MEM_PIC=="")?"default.png":$memRow->MEM_PIC;
-=======
-<<<<<<< HEAD
-	    $_SESSION["memPic"] = ($memRow->MEM_PIC=="")?"default.png":$memRow->MEM_PIC;
-=======
-	    
-
-	    
-	    $_SESSION["memPic"] = $memRow->MEM_PIC==""?"default.png":$memRow->MEM_PIC;
->>>>>>> 8e2d0a255150d6e101a7d1f7daa9c3a562c53b9a
->>>>>>> e6f66a8320ec0a5abd1550a8751370615fea6523
-	    // header("Location:../../../homepage.php");
 	}
 }catch(PDOException $ex){
 	echo "資料庫操作失敗,原因：",$ex->getMessage(),"<br>";
