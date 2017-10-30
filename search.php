b208583b7b6b22b6e8e41520da3a6933f9f93180
adam <spm770311@gmail.com>
2017-10-30 19:38:45 +0800

    no message

--- search.php
+++ search.php
@@ -52,7 +52,7 @@ try{
 		$shopType=0;  //店家 0  胖小車1
 	
 
-		$searchsql="SELECT s.SI_NUM, s.SI_NAME,s.SI_TYPE,s.SI_LNG,s.SI_LAT,s.SI_POSITION,s.SI_ADDR,s.SI_STARTTIME,s.SI_ENDTIME,s.SI_BIMG_1,s.SI_PHONE,s.SI_AVG_REVIEW,COUNT(f.MEM_NO) top FROM store_imformation s LEFT JOIN follow f ON f.SI_NUM=s.SI_NUM LEFT JOIN reviews r ON r.SI_NUM = s.SI_NUM WHERE  s.SI_TYPE='$shopType' ";	
+		$searchsql="SELECT s.SI_NUM, s.SI_NAME,s.SI_TYPE,s.SI_LNG,s.SI_LAT,s.SI_POSITION,s.SI_ADDR,s.SI_STARTTIME,s.SI_ENDTIME,s.SI_BIMG_1,s.SI_PHONE,s.SI_AVG_REVIEW,COUNT(f.MEM_NO) top FROM store_imformation s LEFT JOIN follow f ON f.SI_NUM=s.SI_NUM LEFT JOIN reviews r ON r.SI_NUM = s.SI_NUM WHERE  s.SI_TYPE='$shopType' and s.SI_SELLSTAY=1 s.SI_CHECKSTAY=1 ";	
 			if ($shopPosition!=='') {
 				$searchsql.=" AND s.SI_POSITION = '$shopPosition'";
 			}
