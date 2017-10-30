676d16e35b900d41d41096aec04e024b6ff4c0a0
jeremidee <jeremidee@gmail.com>
2017-10-30 14:53:18 +0800

    修正首頁胖小車地圖

--- php/pdo/connectPDO.php
+++ php/pdo/connectPDO.php
@@ -5,13 +5,6 @@
 	$user = "root";
 	$password = "root";
 
-<<<<<<< HEAD
-	$dsn = "mysql:host=localhost;dbname=trepun;port=3306;charset=utf8";
-	$user = "TREPUN";
-	$password = "1234";
-=======
->>>>>>> a12a560f36a3182633d470094fd871a854f2bf29
-
 	$options = array( PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION);
 	$connectPDO = new PDO($dsn, $user, $password, $options);
  ?>
