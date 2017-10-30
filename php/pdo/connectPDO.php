450eb938a3bba62d9a540b8878c16390e6bbb7e3
jeremidee <jeremidee@gmail.com>
2017-10-30 21:43:05 +0800

    no message

--- php/pdo/connectPDO.php
+++ php/pdo/connectPDO.php
@@ -1,8 +1,8 @@
 <?php 
 
-	$dsn ="mysql:host=localhost;dbname=trepun;port=3306;charset=utf8";
+	$dsn ="mysql:host=localhost;dbname=trepun;port=8889;charset=utf8";
 	$user = "root";
-	$password = "zxc123";
+	$password = "root";
 
 	$options = array( PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION);
 	$connectPDO = new PDO($dsn, $user, $password, $options);
