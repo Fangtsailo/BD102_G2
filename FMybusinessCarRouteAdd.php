7e245a96501e99ead0f087dbc87d05eb969674b8
adam <spm770311@gmail.com>
2017-11-01 01:15:30 +0800

    no message

--- FMybusinessCarRouteAdd.php
+++ FMybusinessCarRouteAdd.php
@@ -803,7 +803,22 @@ session_start();
 
    	function initMap1() {
         map1 = new google.maps.Map(document.getElementById('map1'), {
-          center: {lat: 24.967779, lng: 121.192124},
+
+          center: <?php 
+					if( isset($BCP_LOCATION[0]) ) {
+						
+						$centerLocation_1=explode(",",$BCP_LOCATION[0]);
+
+						
+
+						echo $centerLocation_1[0];
+
+					}else{
+						echo '{lat: 24.967779, lng: 121.192124}';
+					}
+
+						?>,
+					
           zoom: 16
         });
         //先輸入地址把地圖 center 移入
